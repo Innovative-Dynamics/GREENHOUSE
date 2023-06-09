42e3dd7a0fc196d3de726ad7073e47d9d33c10d3
lucabonori <129528480+lucabonori@users.noreply.github.com>
2023-06-09 15:04:48 +0200

    Update stepper.c
    modificato relazione angle-EC

--- Core/Src/stepper.c
+++ Core/Src/stepper.c
@@ -7,6 +7,10 @@
 
 #include "stepper.h"
 
+float deltaEC = 0;
+float angle = 0;
+float coeff_angle = 720;
+
 void stepper_set_rpm(int rpm)  // Set rpm--> max 13, min 1,,,  went to 14 rev/min
 {
 	delay_us(60000000 / stepsperrev / rpm);
@@ -76,32 +80,36 @@ void stepper_half_drive(int step)
 void stepper_step_angle(int direction, int rpm)
 {
 
-	float deltaEC = EC_SETPOINT - EC;
+	deltaEC = EC_SETPOINT - EC;
 
 	char data1[32];
 	sprintf(data1, "deltaEC: %f \n\r", deltaEC);
 	HAL_UART_Transmit(&huart2, (uint8_t*)data1, strlen(data1), HAL_MAX_DELAY);
 
-	float angle = 720 * deltaEC;
+	angle = coeff_angle * deltaEC;
+	
+	if (angle > 0)
+    	{
 
-	int numberofsequences = (int)(angle / anglepersequence);
+		int numberofsequences = (int)(angle / anglepersequence);
 
-	for (int seq = 0; seq < numberofsequences; seq++)
-	{
-		if (direction == 0)  // for clockwise
+		for (int seq = 0; seq < numberofsequences; seq++)
 		{
-			for (int step = 7; step >= 0; step--)
+			if (direction == 0)  // for clockwise
 			{
-				stepper_half_drive(step);
-				stepper_set_rpm(rpm);
+				for (int step = 7; step >= 0; step--)
+				{
+					stepper_half_drive(step);
+					stepper_set_rpm(rpm);
+				}
 			}
-		}
-		else if (direction == 1)  // for anti-clockwise
-		{
-			for (int step = 0; step < 8; step++)
+			else if (direction == 1)  // for anti-clockwise
 			{
-				stepper_half_drive(step);
-				stepper_set_rpm(rpm);
+				for (int step = 0; step < 8; step++)
+				{
+					stepper_half_drive(step);
+					stepper_set_rpm(rpm);
+				}
 			}
 		}
 	}
